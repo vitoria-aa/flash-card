3c82f981eff060b27a9cf569bc1faab79f14801a
vitoria-aa <vitoria.alves.assis@escola.pr.gov.br>
2025-08-21 11:36:50 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+'Programação',
+'O que é HTML?',
+'O HTML (Linguagem de Marcação de HiperTexto)'
+)
+criaCartao(
+'Programação',
+'O que é CSS?',
+'O CSS é uma linguagem de estilização.'
+)
+criaCartao(
+'Programação',
+'O que é JavaScript?',
+'O JavaScript é uma linguagem de programação.'
+)
+criaCartao(
+'Programação',
+'O que é Front-end?',
+' O Front-end compreende a parte visual de sites e aplicações.'
+)
+criaCartao(
+'Programação',
+'O que é Back-end?',
+'O Back-end compreende tudo aquilo que tem por trás de uma aplicação.'
+)
+criaCartao(
+'Programação',
+'O que é Full Stack?',
+
+'A Full Stack developer é uma pessoa que trabalha tanto com Front-end quanto com Back-
+end.'
+
+)
